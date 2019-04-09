a1f0ab806bdf85c0afec5cbff5dcf1b1a9875e85
Thomas Arts <thomas.arts@robuster.software>
2019-04-09 16:23:01 +0200

    Compare Trace and original
    run signal_extract_concretize:start().

---
+++ signal_extract/src/concrete_noise.erl
@@ -0,0 +1,33 @@
+%%% @author Thomas Arts
+%%% @doc  This program describes a conretization of the output of a noise
+%%%       symbolic reference specification into commands that occur in an
+%%%       abstracted trace.
+%%%
+%%% @end
+%%% Created :  9 Apr 2019 by Thomas Arts
+
+-module(concrete_noise).
+
+-compile([export_all, nowarn_export_all]).
+
+'MERGE'(Bin1, Bin2) ->
+    {signal_binary_ops, merge, [Bin1, Bin2]}.
+
+'PUBLIC-KEY'(X) ->
+    io:format("X = ~p\n", [X]),
+    {enacl, crypto_sign_ed25519_public_to_curve25519, [{signal_binary_ops, extract, [X, 32, 32]}]}.
+
+'GENERATE_KEYPAIR'(_) ->
+    {enacl, crypto_sign_ed25519_keypair, []}.
+
+'ENCRYPT'(X, Nonce, Y, Payload) ->
+    {enacl, aead_chacha20poly1305_encrypt, [X, Nonce, Y, Payload]}.
+
+'TRUNCATE'(Bin, Length) ->
+    {signal_binary_ops, truncate, [Bin, Length]}.
+
+'HASH'(T) ->
+    {enacl,generichash, [64, T]}.
+
+%% 'PAYLOAD'(X) ->
+%%     X.
---
+++ signal_extract/src/signal_extract_concretize.erl
@@ -0,0 +1,63 @@
+%%% @author Thomas Arts
+%%% @doc Apply a description to concretize to a symbolic reference trace
+%%%
+%%% @end
+%%% Created :  9 Apr 2019 by Thomas Arts <thomas@SpaceGrey.local>
+
+-module(signal_extract_concretize).
+
+-compile([export_all]).
+
+start() ->
+    start(concrete_noise).
+
+start(Concretizer) ->
+    {SemanticTerm, TraceTerm} = read(),
+    Concrete = concretize(SemanticTerm, Concretizer),
+    io:format("****************************************************\n"),
+    difference(Concrete, TraceTerm).
+
+concretize(List, Concretizer) when is_list(List) ->
+    [ concretize(E, Concretizer) || E <- List ];
+concretize({Fun, Args}, Concretizer) ->
+    ConcreteArgs = concretize(Args, Concretizer),
+    try apply(Concretizer, Fun, ConcreteArgs)
+    catch _:_ ->
+            io:format("Error for ~p\n", [Fun]),
+            {Fun, ConcreteArgs}
+    end;
+concretize(X, _) ->
+    io:format("Not done ~p\n", [X]),
+    X.
+
+
+difference({F, M, As}, {F, M, Bs}) when length(As) == length(Bs) ->
+    {F, M, [difference(A, B) || {A, B} <- lists:zip(As, Bs)]};
+difference(A, A) ->
+    A;
+difference(A, B) ->
+    {A, '/=',  B}.
+
+
+read() ->
+  TraceTerm = signal_extract:analyze_trace_file("enoise.trace"),
+  {wrote, ST} = noise:test(),
+  SemanticTerm =
+    case ST of
+      _ when is_list(ST) ->
+        %% nicer to reverse the list and foldl over the tl with hd as
+        %% first Acc (or simply define a recursive function that is
+        %% immediately obvious
+        lists:foldr
+          (fun (Write, Acc) ->
+               if
+                 Acc == undefined ->
+                   Write;
+                 true ->
+                   noise:'MERGE'(Write, Acc)
+               end
+           end, undefined, ST);
+      Term ->
+        Term
+    end,
+    {SemanticTerm, TraceTerm}.
